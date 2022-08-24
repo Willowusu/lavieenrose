ca02efd5d497bd2a4533a6bb7b1d346f2edbedfc
William Kwadwo Owusu <williamowusu1998@gmail.com>
2022-08-24 10:44:48 +0000

    room reservation commit

--- routes/index.js
+++ routes/index.js
@@ -19,27 +19,28 @@ router.get("/about", function (req, res, next) {
 
 /* GET home page. */
 router.get("/rooms", async function (req, res, next) {
-  var rooms = await Room.find().lean()
+  var rooms = await Room.find().lean();
   res.render("rooms", { room: rooms });
 });
 
 /* GET room information. */
 router.get("/room", async function (req, res, next) {
-  var id = helper.deepSanitize(req.query.id)
-  var roomInfo = await Room.findOne({_id:id}).lean()
+  var id = helper.deepSanitize(req.query.id);
+  var roomInfo = await Room.findOne({ _id: id }).lean();
   res.render("room-information", { room: roomInfo });
 });
 
 /* Reservation endpoints */
-router.get("/reservation", function (req, res, next) {
-  res.render("reservation", { title: "Express" });
+router.get("/reservation", async function (req, res, next) {
+  var rooms = await Room.find().lean();
+  res.render("reservation", { room: rooms });
 });
 
 router.post("/reserve-room", async function (req, res, next) {
   var details = helper.deepSanitize(req.body);
   var code = randomstring.generate(7);
   //TODO: check if code is already in use
-  //TODO: Show receipt like feature and amount to pay
+  //TODO: Show receipt-like feature and amount to pay
   try {
     await Reservation.create({
       firstName: details.fname,
@@ -47,7 +48,7 @@ router.post("/reserve-room", async function (req, res, next) {
       email: details.email,
       room: details.room,
       arrivalDate: details.arrivalDate,
-      departureDate: details.arrivalDate,
+      departureDate: details.departureDate,
       adults: details.adults,
       children: details.children,
       code: code,
@@ -57,7 +58,9 @@ router.post("/reserve-room", async function (req, res, next) {
     return res.status(500).send("Error reserving room");
   }
 
-  return res.status(200).send("Room reserved successfully");
+  let reservationInfo = await Reservation.findOne({code: code}).lean();
+
+  return res.status(200).render("receipt", { room: reservationInfo});
 });
 
 /* GET home page. */
